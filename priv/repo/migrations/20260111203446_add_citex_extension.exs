5d5b75bf31ebe0527b9608d885390cb3dde4f745
Arkadiusz Plichta <el.pikel@gmail.com>
2026-01-12 22:17:45 +0100

    added subscibers migration

--- priv/repo/migrations/20260111203446_add_citex_extension.exs
+++ priv/repo/migrations/20260111203446_add_citex_extension.exs
@@ -2,6 +2,6 @@ defmodule PrzetargowyPrzeglad.Repo.Migrations.AddCitexExtension do
   use Ecto.Migration
 
   def change do
-    execute "CREATE EXTENSION IF NOT EXISTS citext;"
+    execute "CREATE EXTENSION IF NOT EXISTS citext", "DROP EXTENSION citext"
   end
 end
