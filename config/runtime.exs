e8c3cc71234363085e45290493e736d1d0ac0844
Arkadiusz Plichta <el.pikel@gmail.com>
2026-01-17 18:26:25 +0100

    remove ssl

--- config/runtime.exs
+++ config/runtime.exs
@@ -33,9 +33,10 @@ if config_env() == :prod do
 
   config :przetargowy_przeglad, PrzetargowyPrzeglad.Repo,
     url: database_url,
-    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-    ssl: true,
-    ssl_opts: [verify: :verify_none]
+    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+
+  # ssl: true,
+  # ssl_opts: [verify: :verify_none]
 
   secret_key_base =
     System.get_env("SECRET_KEY_BASE") ||
