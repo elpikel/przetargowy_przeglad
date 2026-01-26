0e385e09cf9b1a7ec5d2c4683ca056d263c7008f
Arkadiusz Plichta <el.pikel@gmail.com>
2026-01-26 23:42:36 +0100

    brevo integration

--- config/runtime.exs
+++ config/runtime.exs
@@ -53,12 +53,64 @@ if config_env() == :prod do
   scheme = System.get_env("PHX_SCHEME", "http")
   url_port = if scheme == "https", do: 443, else: 80
 
+  # ## Configuring the mailer
+  #
+  # In production you need to configure the mailer to use a different adapter.
+  # Here is an example configuration for Mailgun:
+  #
+  #     config :przetargowy_przeglad, PrzetargowyPrzeglad.Mailer,
+  #       adapter: Swoosh.Adapters.Mailgun,
+  #       api_key: System.get_env("MAILGUN_API_KEY"),
+  #       domain: System.get_env("MAILGUN_DOMAIN")
+  #
+  # Most non-SMTP adapters require an API client. Swoosh supports Req, Hackney,
+  # and Finch out-of-the-box. This configuration is typically done at
+  # compile-time in your config/prod.exs:
+  #
+  config :przetargowy_przeglad, PrzetargowyPrzeglad.Mailer,
+    adapter: Swoosh.Adapters.Brevo,
+    api_key: System.get_env("BREVO_API_KEY") || raise("BREVO_API_KEY missing")
+
   config :przetargowy_przeglad, PrzetargowyPrzeglad.Repo,
     # ssl: true,
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+
+    # ## SSL Support
     # For machines with several cores, consider starting multiple pools of `pool_size`
+    #
+
     # pool_count: 4,
+    # To get SSL working, you will need to add the `https` key
+    # to your endpoint configuration:
+    #
+    #     config :przetargowy_przeglad, PrzetargowyPrzegladWeb.Endpoint,
+    #       https: [
+    #         ...,
+    #         port: 443,
+    #         cipher_suite: :strong,
+    #         keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
+    #         certfile: System.get_env("SOME_APP_SSL_CERT_PATH")
+    #       ]
+    #
+    # The `cipher_suite` is set to `:strong` to support only the
+    # latest and more secure SSL ciphers. This means old browsers
+    # and clients may not be supported. You can set it to
+    # `:compatible` for wider support.
+    #
+    # `:keyfile` and `:certfile` expect an absolute path to the key
+    # and cert in disk or a relative path inside priv, for example
+    # "priv/ssl/server.key". For all supported SSL configuration
+    # options, see https://hexdocs.pm/plug/Plug.SSL.html#configure/1
+    #
+    # We also recommend setting `force_ssl` in your config/prod.exs,
+    # ensuring no data is ever sent via http, always redirecting to https:
+    #
+    #     config :przetargowy_przeglad, PrzetargowyPrzegladWeb.Endpoint,
+    #       force_ssl: [hsts: true]
+    #
+    # Check `Plug.SSL` for all available options in `force_ssl`.
+
     socket_options: maybe_ipv6
 
   config :przetargowy_przeglad, PrzetargowyPrzegladWeb.Endpoint,
@@ -75,55 +127,8 @@ if config_env() == :prod do
       "//*.sslip.io"
     ]
 
-  # ## SSL Support
-  #
-  # To get SSL working, you will need to add the `https` key
-  # to your endpoint configuration:
-  #
-  #     config :przetargowy_przeglad, PrzetargowyPrzegladWeb.Endpoint,
-  #       https: [
-  #         ...,
-  #         port: 443,
-  #         cipher_suite: :strong,
-  #         keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-  #         certfile: System.get_env("SOME_APP_SSL_CERT_PATH")
-  #       ]
-  #
-  # The `cipher_suite` is set to `:strong` to support only the
-  # latest and more secure SSL ciphers. This means old browsers
-  # and clients may not be supported. You can set it to
-  # `:compatible` for wider support.
-  #
-  # `:keyfile` and `:certfile` expect an absolute path to the key
-  # and cert in disk or a relative path inside priv, for example
-  # "priv/ssl/server.key". For all supported SSL configuration
-  # options, see https://hexdocs.pm/plug/Plug.SSL.html#configure/1
-  #
-  # We also recommend setting `force_ssl` in your config/prod.exs,
-  # ensuring no data is ever sent via http, always redirecting to https:
-  #
-  #     config :przetargowy_przeglad, PrzetargowyPrzegladWeb.Endpoint,
-  #       force_ssl: [hsts: true]
-  #
-  # Check `Plug.SSL` for all available options in `force_ssl`.
-
   config :przetargowy_przeglad, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
-  # ## Configuring the mailer
-  #
-  # In production you need to configure the mailer to use a different adapter.
-  # Here is an example configuration for Mailgun:
-  #
-  #     config :przetargowy_przeglad, PrzetargowyPrzeglad.Mailer,
-  #       adapter: Swoosh.Adapters.Mailgun,
-  #       api_key: System.get_env("MAILGUN_API_KEY"),
-  #       domain: System.get_env("MAILGUN_DOMAIN")
-  #
-  # Most non-SMTP adapters require an API client. Swoosh supports Req, Hackney,
-  # and Finch out-of-the-box. This configuration is typically done at
-  # compile-time in your config/prod.exs:
-  #
-  #     config :swoosh, :api_client, Swoosh.ApiClient.Req
   #
   # See https://hexdocs.pm/swoosh/Swoosh.html#module-installation for details.
 end
